5e0bb031a43310cc5ab02d19d46dc29e2e92e7a7
Sistemas UAD <sistemas_uad@hotmail.com>
2022-05-30 13:17:41 -0500

    MRFR initial

---
+++ NewReportesControlEscolar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewReportesControlEscolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
